dac06ac33760ef24ff1866995157ea3be26e6cb2
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2025-11-05 11:15:07 +0200

    change middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,27 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest } from 'next/server';
 
-export default createMiddleware({
+const intlMiddleware = createMiddleware({
   locales: ['bg', 'en'],
-  defaultLocale: 'bg'
+  defaultLocale: 'bg',
+  localePrefix: 'always'
 });
 
+export default function middleware(request: NextRequest) {
+  return intlMiddleware(request);
+}
+
 export const config = {
-  matcher: ['/', '/(bg|en)/:path*']
+  matcher: [
+    // Enable a redirect to a matching locale at the root
+    '/',
+    
+    // Set a cookie to remember the previous locale for
+    // all requests that have a locale prefix
+    '/(bg|en)/:path*',
+    
+    // Enable redirects that add missing locales
+    // (e.g. `/pathnames` -> `/en/pathnames`)
+    '/((?!api|_next|_vercel|.*\\..*).*)'
+  ]
 };
